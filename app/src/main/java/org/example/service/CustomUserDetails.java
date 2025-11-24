395ef03ecdacbc698e63cb78993f2d977bcc5fea
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-24 17:53:43 +0530

    Created a custom entity to save all the info of the user and also their auths/roles using spring security

---
+++ app/src/main/java/org/example/service/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package org.example.service;
+
+import org.example.entities.UserInfo;
+import org.example.entities.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails extends UserInfo implements UserDetails {
+
+    private String username;
+    private String password;
+    Collection<? extends GrantedAuthority> authorities;
+
+    // Constructor to save all the roles from the User role and also from the userinfo and then save it to the authorities list
+    public CustomUserDetails(UserInfo byUsername) {
+        this.username = byUsername.getUsername();
+        this.password = byUsername.getPassword();
+        List<GrantedAuthority> auths = new ArrayList<>();
+
+        for (UserRole role : byUsername.getRoles()) {
+            auths.add(new SimpleGrantedAuthority(role.getName().toUpperCase()));
+        }
+        this.authorities = auths;
+    }
+
+    //Just overriding all the methods of the interface that we implemented of Spring Security, UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
