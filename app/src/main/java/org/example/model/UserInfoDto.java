b8b47c6528e2ffdd23232e30ecbeaf992b6336af
JeffTunner <abhinavkaushik05@gmail.com>
2025-11-28 14:55:50 +0530

    Created a refresh Token service

---
+++ app/src/main/java/org/example/model/UserInfoDto.java
@@ -0,0 +1,18 @@
+package org.example.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.entities.UserInfo;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Getter
+@Setter
+public class UserInfoDto extends UserInfo {
+
+    private String firstName;
+    private String lastName;
+    private Long phoneNumber;
+    private String email;
+}
